4a8ddb3061aef81730d1296e5e4914f0cc17986d
Travis Cummings <travis@istalusconsulting.com>
2016-01-28 15:47:59 -0700

    Refactored hsp-reference-apps to be hspc-reference-apps

---
+++ src/main/resources/db/openidconnect/mysql/test-clients-rollback.sql
@@ -0,0 +1,33 @@
+USE oic;
+
+SET AUTOCOMMIT = 0;
+
+START TRANSACTION;
+
+-- Test Client
+DELETE FROM client_grant_type WHERE owner_id = (SELECT id from client_details where client_id = 'test_client');
+DELETE FROM client_scope WHERE owner_id = (SELECT id from client_details where client_id = 'test_client');
+DELETE FROM client_redirect_uri WHERE owner_id = (SELECT id from client_details where client_id = 'test_client');
+DELETE FROM client_details WHERE client_id = 'test_client';
+
+-- Test Client using JWT
+DELETE FROM client_grant_type WHERE owner_id = (SELECT id from client_details where client_id = 'test_client_jwt');
+DELETE FROM client_scope WHERE owner_id = (SELECT id from client_details where client_id = 'test_client_jwt');
+DELETE FROM client_redirect_uri WHERE owner_id = (SELECT id from client_details where client_id = 'test_client_jwt');
+DELETE FROM client_details WHERE client_id = 'test_client_jwt';
+
+-- Standalone Patient Test Client
+DELETE FROM client_grant_type WHERE owner_id = (SELECT id from client_details where client_id = 'standalone_patient_test_client');
+DELETE FROM client_scope WHERE owner_id = (SELECT id from client_details where client_id = 'standalone_patient_test_client');
+DELETE FROM client_redirect_uri WHERE owner_id = (SELECT id from client_details where client_id = 'standalone_patient_test_client');
+DELETE FROM client_details WHERE client_id = 'standalone_patient_test_client';
+
+-- Standalone Clinician Test Client
+DELETE FROM client_grant_type WHERE owner_id = (SELECT id from client_details where client_id = 'standalone_clinical_test_client');
+DELETE FROM client_scope WHERE owner_id = (SELECT id from client_details where client_id = 'standalone_clinical_test_client');
+DELETE FROM client_redirect_uri WHERE owner_id = (SELECT id from client_details where client_id = 'standalone_clinical_test_client');
+DELETE FROM client_details WHERE client_id = 'standalone_clinical_test_client';
+
+COMMIT;
+
+SET AUTOCOMMIT = 1;
